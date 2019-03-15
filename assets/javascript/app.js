1a1ced05dc62193aa51233b5ec5f15136415b71c
Glacian22 <47679905+Glacian22@users.noreply.github.com>
2019-03-15 13:40:38 -0700

    wip

---
+++ assets/javascript/app.js
@@ -0,0 +1,6 @@
+var config = {
+    apiKey: "AIzaSyA5eYKsB8T2q6rMGdKSvac6eQsWTzsZEjE",
+    authDomain: "fir-recent-user.firebaseapp.com",
+    databaseURL: "https://fir-recent-user.firebaseio.com",
+    storageBucket: "fir-recent-user.appspot.com"
+  };
