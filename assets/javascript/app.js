7e5450e4581fa9eaf2660dd964308f05e5ba4a4f
Glacian22 <47679905+Glacian22@users.noreply.github.com>
2019-03-16 17:50:06 -0700

    semi-functional, no end conditions but does clear server at end of round

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,6 +14,14 @@ $(document).ready(function () {
     var database = firebase.database();
     //client tracking of numPlayers, which is on firebase
     var players;
+    var gameState;
+
+    var myScore = 0;
+    var theirScore = 0;
+    var myChoice = "";
+    var theirChoice = "";
+
+    var playerID;
 
     //tell player they're waiting for an opponent
     $("h2").text("Waiting for another player to join");
@@ -25,15 +33,110 @@ $(document).ready(function () {
     }).then(function () {
         if (players < 2) { //if there are less than 2 players
             players++;
+
+            //set playerID (first player or second), update server
+            console.log("updating numPlayers");
+            playerID = players;
+            console.log("playerID: ", playerID);
             database.ref().child("numPlayers").set(players);
+        } else {
+            //multiple game instances not yet implemented, so if there are already 2 players, apologize to the user
+            $("h2").text("Sorry, game is full");
         }
     })
 
-    //database listener to check for numPlayers = 2, to start game, and compare RPS values during gameplay
+    //database change listener 
     database.ref().on("child_changed", function (snapshot) {
-        console.log("child changed triggered");
-        var data = snapshot.val();
+        //if the child changed was the player count
+        if (typeof snapshot.val() === "number") {
+            players = snapshot.val();
 
+            //start game if there are 2 players
+            if (players === 2) {
+                gameState = "running";
+                console.log("gamestate: ", gameState);
+                $("h2").text("Alright, opponent found! Make your choice!");
+            }
+        } else {
+            //it was an RPS value
+            //if it was your opponent's choice, store it locally
+            if (snapshot.key !== "player" + playerID + "Choice") {
+                theirChoice = snapshot.val();
+                $(".their-choice").hide();
+                $(".their-choice").text("They chose: " + theirChoice);
+
+            }
+
+            //if both players have sent their choice to the server, evaluate
+            database.ref().once("value", function (bigSnapshot) {
+                var choice1 = bigSnapshot.val().player1Choice;
+                var choice2 = bigSnapshot.val().player2Choice;
+                if (choice1 !== "" && choice2 !== "") {
+                    console.log("comparing choices")
+                    comparePlayerChoices();
+                }
+            })
+
+            //watch for end of round
+        }
     })
 
+    //capture image clicks
+    //if string is empty, update choice
+    $("img").on("click", function () {
+        if (!myChoice) {
+            myChoice = $(this).attr("value");
+            $(".my-choice").text("I chose: " + myChoice);
+            database.ref().child("player" + playerID + "Choice").set(myChoice);
+        }
+    })
+
+
+    function comparePlayerChoices() {
+        //write function comparing player choices
+        //reveal opponent's choice
+        $(".their-choice").show();
+        console.log("my choice: ", myChoice, "their choice: ", theirChoice)
+        if (myChoice === theirChoice) {
+            $("h2").text("It's a tie! Choose again!")
+            resetChoices();
+        } else if (myChoice === "scissors" && theirChoice === "paper") {
+            getPoint();
+        } else if (myChoice === "paper" && theirChoice === "rock") {
+            getPoint();
+        } else if (myChoice === "rock" && theirChoice === "scissors") {
+            getPoint();
+        } else {
+            $("h2").text("So sad, so sad. You can do better.")
+            theirScore++;
+            updateScores();
+            resetChoices();
+        }
+    }
+
+    function getPoint() {
+        myScore++;
+        resetChoices();
+        updateScores();
+        database.ref().child("player1Choice").set("");
+        database.ref().child("player2Choice").set("");
+        $("h2").text("You chose wisely! You're on your way to victory!")
+
+    }
+    function updateScores() {
+        $(".my-score").text("My Score: " + myScore);
+        $(".their-score").text("Their Score: " + theirScore);
+    }
+
+    function resetChoices() {
+        //doesn't matter which resets which, as long as they're both cleared
+        database.ref().child("player1Choice").set("")
+            .then(function () {
+                myChoice = "";
+            })
+        database.ref().child("player2Choice").set("")
+            .then(function () {
+                theirChoice = "";
+            });
+    }
 })
